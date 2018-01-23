153b70f64c09224879d0a7f6ddac532196336d5e
Harsh Thakkar <harshthakkar46@gmail.com>
2018-01-23 09:25:19 +0530

    Added more projects and styling changes

--- js/main.js
+++ js/main.js
@@ -1,30 +1,18 @@
-console.log("HE110 CREPPER /(*_*)/");
+var projects = (function() {
+	var json = null;
+	$.ajax({
+		'async': false,
+		'global': false,
+		'url': "js/projects.json",
+		'dataType': "json",
+		'success': function (data) {
+			json = data;
+		}
+	});
+	return json;
+})();
 
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyDV48Vgcfz3B7pjGOsxuHCjZ1yh9SZa5WU",
-  authDomain: "photos-d71d1.firebaseapp.com",
-  databaseURL: "https://photos-d71d1.firebaseio.com/",
-  storageBucket: "gs://photos-d71d1.appspot.com"
-};
-
-firebase.initializeApp(config);
-
-// Get a reference to the storage service, which is used to create references in your storage bucket
-var storage = firebase.storage();
-// Create a storage reference from our storage service
-var storageRef = storage.ref();
-// Photos reference
-var photosRef = storageRef.child('photography');
-
-var images = ["4.jpg", "5.jpg", "10.jpg", "11.jpg"];
-
-for (var i = 0; i < images.length; i++) {
-  photosRef.child(images[i]).getDownloadURL().then(function(url) {
-    var image = new Image();
-    image.src = url;
-    image.className = "single-image img-responsive";
-    $(".images-main").append(image);
-  });
-}
-
+$.each(projects, function (index, project) {
+	var li = '<li><div><h4 class="project-title"><a href="' + project.url + '" target="_blank">' + project.name + '</a></h4><p class="project-duration">' + project.language + ' // ' + project.duration + '</p></div></li>'
+	$('#projects ul').append(li);
+});
